9e4a07927e00064ffff4fce993cb50f4ecafa608
ASHURASINGLE <darkness789867@gmail.com>
2025-05-29 16:58:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQRhzJWx2XYIdM30681Zoc6QI_t-IhzNw",
+  authDomain: "ashurastore.firebaseapp.com",
+  databaseURL: "https://ashurastore-default-rtdb.firebaseio.com",
+  projectId: "ashurastore",
+  storageBucket: "ashurastore.firebasestorage.app",
+  messagingSenderId: "860052192821",
+  appId: "1:860052192821:android:3b6739051a144ed25cbb7c"
+};
+
+firebase.initializeApp(firebaseConfig);
